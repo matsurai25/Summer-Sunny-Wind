5b49f170c1c355184c044f152f0b1b99e8a2fa4e
matsurai25 <souta.m825@gmail.com>
2015-08-27 11:05:40 +0900

    A-1まで

--- color/colors.jsx
+++ color/colors.jsx
@@ -24,23 +24,43 @@ function getColorsKey(obj){
     }
     return colorsKey
 }
+function getColorsKeyFreeGray(obj){
+    var colorsKey = [];
+    for(var j in obj){
+        if(j=="gray"){continue}
+        colorsKey.push(j)
+    }
+    return colorsKey
+}
 
 function randomColor(time,slowly){
     if(typeof(slowly) == "undefined") {
         slowly = 1;
     }
-    var t = timeToFrames(time, fps = 1.0 / thisComp.frameDuration, isDuration = false)
+    var t = Math.abs(timeToFrames(time, fps = 1.0 / thisComp.frameDuration, isDuration = false))
     var keys = getColorsKey(colors);
     var size = countColors(colors);
     var num = Math.floor((t / slowly) % size);
     return colors[keys[num]]
 }
+function randomColorFreeGray(time,slowly){
+    if(typeof(slowly) == "undefined") {
+        slowly = 1;
+    }
+    var t = Math.abs(timeToFrames(time, fps = 1.0 / thisComp.frameDuration, isDuration = false))
+    var keys = getColorsKeyFreeGray(colors);
+    var size = countColors(colors)-1;
+    var num = Math.floor((t / slowly) % size);
+    return colors[keys[num]]
+}
 
 var colors = {};
-colors.blue = color16("205cde");
 colors.darkblue = color16("103896");
-colors.green = color16("1dbdcd");
-colors.pink = color16("e22d6e");
 colors.yellow = color16("FFE900");
-colors.orange = color16("FFA541");
+colors.green = color16("1dbdcd");
+colors.lightblue = color16("8ceff5");
+colors.pink = color16("e22d6e");
 colors.gray = color16("363636");
+colors.blue = color16("205cde");
+colors.orange = color16("FFA541");
+colors.white = color16("ffffff");
